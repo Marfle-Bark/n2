058bbcfddc6d8096ece5aa86aff1d24fcd8dd6c1
Ethan Busbee <ethan.busbee@gmail.com>
2015-06-22 13:30:49 -0500

    Created file Letter.py and wrote some documentation into CharNeuron.py.

--- CharNeuron.py
+++ CharNeuron.py
@@ -1,1 +1,7 @@
 __author__ = 'colin'
+
+# CharNeuron is intended to take in a character,
+# mutate it by connecting with other CharNeurons,
+# and then output a new character.
+
+# Mutations can currently only involve translations forward or backward in the alphabet.
