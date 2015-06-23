fc49decf179767f13aa24b315fb70e186dd621b0
Ethan Busbee <ethan.busbee@gmail.com>
2015-06-22 21:31:59 -0500

    Started working on CNLayer.py.

--- CNLayer.py
+++ CNLayer.py
@@ -1,1 +1,18 @@
 __author__ = 'Ethan Busbee'
+
+from CharNeuron import CharNeuron
+
+class CNLayer:
+
+    def __init__(self):
+        self._neurons = []
+
+    def getCNs(self):
+        return list(self._neurons)
+
+    def addCN(self, neuron):
+        self._neurons.append(neuron)
+
+    def removeCN(self, neuron):
+        self._neurons.remove(neuron)
+
