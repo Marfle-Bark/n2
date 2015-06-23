92081c1368c74b2e844131c9b2fd91117e0e8688
Ethan Busbee <ethan.busbee@gmail.com>
2015-06-22 22:42:53 -0500

    Implemented large parts of CNLayer.py.
    Rewrote __init__().
    Added __str__().
    Added set/getInput().
    Added setupCNs().
    Added randomizeConnections().
    Added fireCNs().
    Added translateCNs().
    Added getOutput().
    Added the most basic of test code.

--- CNLayer.py
+++ CNLayer.py
@@ -1,11 +1,30 @@
 __author__ = 'Ethan Busbee'
 
 from CharNeuron import CharNeuron
+import random
 
 class CNLayer:
 
-    def __init__(self):
+    def __init__(self, input = None):
         self._neurons = []
+
+        if input is None:
+            input = ""
+        self._input = input
+        self._output = ""
+
+        if self._input is not "":
+            self.setupCNs()
+            self.randomizeConnections(2 * len(self._input))
+            self.fireCNs()
+            self.translateCNs()
+
+    def __str__(self):
+        out = "Layer Neurons:\n"
+        for cn in self.getCNs():
+            out += str(cn)
+            out += "\n"
+        return out
 
     def getCNs(self):
         return list(self._neurons)
@@ -16,3 +35,44 @@ class CNLayer:
     def removeCN(self, neuron):
         self._neurons.remove(neuron)
 
+    def setInput(self, string):
+        self._input = string
+
+    def getInput(self):
+        return self._input
+
+    def setupCNs(self):
+        self._neurons = []
+        for char in self._input:
+            self.addCN(CharNeuron(char))
+
+    def randomizeConnections(self, count):
+        cnCount = len(self.getCNs()) - 1
+        if cnCount < 2:
+            return
+        for i in range(0, count):
+            lucky1 = random.randint(0, cnCount)
+            lucky2 = random.randint(0, cnCount)
+            while lucky2 is lucky1:
+                lucky2 = random.randint(0, cnCount)
+            strength = random.randint(0, 26)
+            self._neurons[lucky1].addOutput(((self._neurons[lucky2]), strength))
+
+    def fireCNs(self):
+        for cn in self._neurons:
+            cn.fire()
+
+    def translateCNs(self):
+        for cn in self._neurons:
+            cn.translate()
+
+        self._output = ""
+        for cn in self._neurons:
+            self._output += cn.getInChar().getLetter()
+
+    def getOutput(self):
+        return self._output
+
+if __name__ == "__main__":
+    cnl = CNLayer("marfle")
+    print cnl
